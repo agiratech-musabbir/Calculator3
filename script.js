498535ffc4c3067b172269cf5426c21383a1fbd2
Musabbir <musabbir.md@agiratech.com>
2024-01-02 18:20:17 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var queue = [];
+var queue = '';
 var input = 0;
 
 function calculateQueue(value) {
@@ -41,10 +41,10 @@ function calculateQueue(value) {
   }
 }
 function addToQueue(input) {
-  queue.push(input);
+  queue += input;
 }
 function clearAll() {
-  queue = [];
+  queue = '';
   input = 0;
   document.getElementById("answer").innerHTML = "0";
 }
@@ -78,4 +78,91 @@ function operatorButton(arg) {
 
     document.getElementById("answer").innerHTML = "-";
   }
+}
+
+const precedence = {
+  '^' : 3,
+  '/' : 2,
+  '*' : 2,
+  '+' : 1,
+  '-' : 1,
+}
+
+function printResult(queue) {
+  console.log(queue);
+  let result = infixToPostfix(queue);
+  console.log(result);
+  document.getElementById("answer").innerHTML = result;
+}
+
+function infixToPostfix(infixExpression) {
+  let lastCharacterIndex = 0;
+  let postfixExpressionList = [];
+  let symbolStack = [];
+  for (let i = 0; i < infixExpression.length; i++) {
+    let currentElement = infixExpression.charAt(i);
+    let operatorString = "^/*+-";
+    let indexOfCurrentElement = i;
+    if (i > 0 && !operatorString.includes(infixExpression[i-1]) && operatorString.includes(currentElement)) {
+      postfixExpressionList.push(infixExpression.substring(lastCharacterIndex, indexOfCurrentElement).trim());
+      lastCharacterIndex = indexOfCurrentElement + 1;
+      insertSymbolIntoPostfixExpressionList(postfixExpressionList, symbolStack, currentElement);
+    } else if (i === infixExpression.length - 1) {
+      postfixExpressionList.push(infixExpression.substring(lastCharacterIndex, indexOfCurrentElement + 1).trim());
+      emptySymbolStack(symbolStack, postfixExpressionList);
+    }
+  }
+  console.log(postfixExpressionList);
+  let result = performCalculation(postfixExpressionList);
+  saveToStorage(infixExpression, result);
+  if (isNaN(result)) return "Malformed Expression";
+  return result;
+}
+
+function insertSymbolIntoPostfixExpressionList(postFixExpressionList, symbolStack, currentElement) {
+  while (
+    symbolStack.length > 0 &&
+    precedence[currentElement] <=
+      precedence[symbolStack[symbolStack.length - 1]]
+  ) {
+    postFixExpressionList.push(symbolStack.pop());
+  }
+  symbolStack.push(currentElement);
+}
+
+function emptySymbolStack(symbolStack, postFixExpressionList) {
+  while (symbolStack.length > 0) {
+    postFixExpressionList.push(symbolStack.pop());
+  }
+}
+
+function performCalculation(postFixExpressionList) {
+  let numberStack = [];
+  while (postFixExpressionList.length > 0) {
+    let firstPoppedElement = postFixExpressionList.shift();
+    if ("^/*+-".includes(firstPoppedElement)) {
+      let secondNumber = parseFloat(numberStack.pop());
+      let firstNumber = parseFloat(numberStack.pop());
+      //console.log(secondNumber + " " + firstNumber);
+      if (firstPoppedElement === "^")
+        numberStack.push(Math.pow(firstNumber, secondNumber));
+      else if (firstPoppedElement === "/")
+        numberStack.push(firstNumber / secondNumber);
+      else if (firstPoppedElement === "*")
+        numberStack.push(firstNumber * secondNumber);
+      else if (firstPoppedElement === "+")
+        numberStack.push(firstNumber + secondNumber);
+      else if (firstPoppedElement === "-")
+        numberStack.push(firstNumber - secondNumber);
+      else return "invalid operator";
+    } else {
+      numberStack.push(firstPoppedElement);
+    }
+  }
+  return numberStack.pop();
+}
+
+function saveToStorage(infixExpression, result) {
+  localStorage.setItem(infixExpression, result);
+  sessionStorage.setItem(infixExpression, result);
 }
