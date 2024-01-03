3db1df8bbcb5dea32c02daff110537b07aa9f27f
Musabbir <musabbir.md@agiratech.com>
2024-01-03 09:49:55 +0530

    This is 3rd commit

--- script.js
+++ script.js
@@ -1,50 +1,12 @@
-var queue = '';
+var queue = "";
 var input = 0;
 
-function calculateQueue(value) {
-  if (input !== 0) {
-    input = parseFloat(input);
-
-    addToQueue(input);
-  }
-  var answer = value[0];
-  var dividedByZero = 0;
-  for (var i = 1; i < value.length; i = i + 2) {
-    switch (queue[i]) {
-      case "+":
-        answer += value[i + 1];
-        break;
-      case "-":
-        answer -= value[i + 1];
-        break;
-      case "/":
-        if (value[i + 1] === 0) dividedByZero = 1;
-        else answer = answer / value[i + 1];
-
-        break;
-      case "*":
-        answer = answer * value[i + 1];
-        break;
-    }
-  }
-
-  answer = answer.toFixed(10);
-  answer = parseFloat(answer);
-  localStorage.setItem(number, answer);
-  if (dividedByZero === 1) {
-    clearAll();
-    document.getElementById("answer").innerHTML = "ERROR";
-  } else {
-    document.getElementById("answer").innerHTML = answer;
-    input = answer;
-    queue = [];
-  }
-}
 function addToQueue(input) {
   queue += input;
 }
+
 function clearAll() {
-  queue = '';
+  queue = "";
   input = 0;
   document.getElementById("answer").innerHTML = "0";
 }
@@ -56,39 +18,24 @@ function numericButton(arg) {
   ) {
     document.getElementById("answer").innerHTML = "";
   }
-
-  if (!(arg === ".") || !input.match(/[.]/)) {
-    input += arg;
-    document.getElementById("answer").innerHTML += arg;
-  }
+  addToQueue(arg);
+  document.getElementById("answer").innerHTML = queue;
 }
 
 function operatorButton(arg) {
-  if (input !== 0 && input !== "-") {
-    input = parseFloat(input);
-
-    addToQueue(input);
-    addToQueue(arg);
-
-    document.getElementById("answer").innerHTML += arg;
-    input = 0;
-  }
-  if (arg == "-" && isNaN(queue[0]) && input !== "-") {
-    input = "-";
-
-    document.getElementById("answer").innerHTML = "-";
-  }
+  addToQueue(arg);
+  document.getElementById("answer").innerHTML = queue;
 }
 
 const precedence = {
-  '^' : 3,
-  '/' : 2,
-  '*' : 2,
-  '+' : 1,
-  '-' : 1,
-}
+  "^": 3,
+  "/": 2,
+  "*": 2,
+  "+": 1,
+  "-": 1,
+};
 
-function printResult(queue) {
+function calculateQueue(queue) {
   console.log(queue);
   let result = infixToPostfix(queue);
   console.log(result);
@@ -103,23 +50,43 @@ function infixToPostfix(infixExpression) {
     let currentElement = infixExpression.charAt(i);
     let operatorString = "^/*+-";
     let indexOfCurrentElement = i;
-    if (i > 0 && !operatorString.includes(infixExpression[i-1]) && operatorString.includes(currentElement)) {
-      postfixExpressionList.push(infixExpression.substring(lastCharacterIndex, indexOfCurrentElement).trim());
+    if (
+      i > 0 &&
+      !operatorString.includes(infixExpression[i - 1]) &&
+      operatorString.includes(currentElement)
+    ) {
+      postfixExpressionList.push(
+        infixExpression
+          .substring(lastCharacterIndex, indexOfCurrentElement)
+          .trim()
+      );
       lastCharacterIndex = indexOfCurrentElement + 1;
-      insertSymbolIntoPostfixExpressionList(postfixExpressionList, symbolStack, currentElement);
+      insertSymbolIntoPostfixExpressionList(
+        postfixExpressionList,
+        symbolStack,
+        currentElement
+      );
     } else if (i === infixExpression.length - 1) {
-      postfixExpressionList.push(infixExpression.substring(lastCharacterIndex, indexOfCurrentElement + 1).trim());
+      postfixExpressionList.push(
+        infixExpression
+          .substring(lastCharacterIndex, indexOfCurrentElement + 1)
+          .trim()
+      );
       emptySymbolStack(symbolStack, postfixExpressionList);
     }
   }
   console.log(postfixExpressionList);
   let result = performCalculation(postfixExpressionList);
   saveToStorage(infixExpression, result);
-  if (isNaN(result)) return "Malformed Expression";
+  if (isNaN(result)) return "Error";
   return result;
 }
 
-function insertSymbolIntoPostfixExpressionList(postFixExpressionList, symbolStack, currentElement) {
+function insertSymbolIntoPostfixExpressionList(
+  postFixExpressionList,
+  symbolStack,
+  currentElement
+) {
   while (
     symbolStack.length > 0 &&
     precedence[currentElement] <=
